dab20c1d11c810f162b00b10de4ff0eeb11b53a4
antonije <antonijepavlovic1@gmail.com>
2020-07-07 20:36:45 +0200

    merge-sort example added

---
+++ sorting-algorithms/merge-sort.js
@@ -0,0 +1,39 @@
+function merge(array1, array2) {
+    const result = [];
+    let i = 0;
+    let j = 0;
+    while (i < array1.length && j < array2.length) {
+        if (array1[i] < array2[j]) {
+            result.push(( array1[i] ));
+            i++
+        } else {
+            result.push(array2[j]);
+            j++
+        }
+    }
+    while (i < array1.length) {
+        result.push(array1[i]);
+        i++
+    }
+    while (j < array2.length) {
+        result.push(array2[j]);
+        j++
+    }
+    return result;
+}
+
+function mergeSort(array) {
+    if (array.length <= 1) {
+        return array;
+    }
+
+    let middle = Math.floor(array.length / 2);
+    let left = mergeSort(array.slice(0, middle));
+    let right = mergeSort(array.slice(middle));
+
+    return merge(left, right);
+}
+
+console.time('mergeSort');
+console.log(mergeSort([ 12, 432, 32, 12, 3, 21, 21 ]));  // mergeSort: 7.798ms
+console.timeEnd('mergeSort');
