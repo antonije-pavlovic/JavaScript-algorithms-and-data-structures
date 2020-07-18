345a2c494f87af1c738132ca29aa8efa467b9989
antonije <antonijepavlovic1@gmail.com>
2020-07-18 14:21:03 +0200

    singly linked list

--- data-structures/singly-linked-lists.js
+++ data-structures/singly-linked-lists.js
@@ -96,15 +96,118 @@ class SingleLinkedList {
         this.length++;
         return true;
     }
+
+    /**
+     *  Retrieve a node by its position in the list
+     */
+
+    get(position) {
+        if (position < 0 || position > this.length) {
+            return null;
+        }
+
+        let counter = 0;
+        let current = this.head;
+
+        while (position !== counter) {
+            current = current.next;
+            counter++;
+        }
+
+        return current;
+    }
+
+    /**
+     *  Change value of node based on its position in the list
+     */
+    set(value, position) {
+        if (position < 0 || position > this.length) {
+            return false;
+        }
+        const targetNode = this.get(position);
+        if (targetNode) {
+            targetNode.value = value;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     *  Adding a node to the list at specific position
+     */
+    insert(value, position) {
+        if (position < 0 || position > this.length) {
+            return false;
+        }
+        if (position === this.length) {
+            return this.push(value);
+        }
+        if (position === 0) {
+            return this.unshift(value);
+        }
+
+        const newNode = new Node(value);
+        const previous = this.get(position - 1);
+
+        newNode.next = previous.next;
+        previous.next = newNode;
+        this.length++;
+
+        return true;
+    }
+
+    /**
+     *  Remove node from list at the specific position
+     */
+    remove(position) {
+        if (position < 0 || position > this.length) {
+            return false;
+        }
+        if (position === 0) {
+            return this.shift()
+        }
+        if (position === this.length - 1) {
+            return this.pop();
+        }
+
+        const previous = this.get(position - 1);
+        previous.next = previous.next.next;
+        this.length--;
+
+        return true;
+    }
+
+    /**
+     * Reversing the list in place
+     */
+    reverse() {
+        let node = this.head;
+        this.head = this.tail;
+        this.tail = this.head;
+
+        let next;
+        let previous = null;
+        for (let i = 0 ; i < this.length ; i++) {
+            next = node.next;
+            node.next = previous;
+
+            previous = node;
+            node = next;
+        }
+
+        return true;
+    }
+
+    /**
+     *  Print all nodes in the list
+     */
+    print() {
+        const arr = [];
+        let node = this.head;
+        for(let i = 0; i < this.length; i++) {
+            arr.push(node);
+            node = node.next;
+        }
+        console.log(arr);
+    }
 }
-
-const list = new SingleLinkedList();
-
-list.push(1);
-
-//console.log(list.pop());
-list.push(2);
-list.unshift(3);
-//console.log(list);
-console.log({ head: list.head });
-console.log({ tail: list.tail });
