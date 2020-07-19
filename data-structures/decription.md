42b98a2c36143cf83f291dc3451e6776e5308c05
antonije <antonijepavlovic1@gmail.com>
2020-07-19 17:44:34 +0200

    doubly linked list

---
+++ data-structures/decription.md
@@ -0,0 +1,27 @@
+# Data structures
+
+### Boundary conditions
+
+**Empty data structure**
+
+- what if we have empty data structure
+- if we trying to remove element from data structure and it is empty what we gonna do?
+- If we want to add something, how does it change addition if data structure is empty vs when something is already there 
+
+**Single element in the data structure**
+
+- what if there is single element in data structure
+- if we removing something, that means we are ending with empty list
+- if we are adding something, what we need to do
+
+**Adding / removing from the beginning of data structure**
+
+- we need to be aware of head pointer and how we gonna handle it
+
+**Adding / removing from the end of data structure**
+
+- we need to think about tail
+
+**Working in the middle**
+
+- most operations is handle here
---
+++ data-structures/doubly-linked-list.js
@@ -0,0 +1,185 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.previous = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    /**
+     *  Adding element on the end of list
+     */
+    push(value) {
+        const newNode = new Node(value);
+
+        if (!this.length) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.previous = this.tail;
+            this.tail = newNode;
+        }
+
+        this.length++;
+        return true;
+    }
+
+    /**
+     *  Remove last element in the list
+     */
+    pop() {
+        if (!this.length) {
+            return false;
+        }
+
+        const previous = this.tail.previous;
+        if (previous) {
+            previous.next = null;
+            this.tail.previous = null;
+            this.tail = previous;
+        } else {
+            this.head = null;
+            this.tail = null;
+        }
+
+        this.length--;
+        return true;
+    }
+
+    /**
+     * Removing a node from the beginning of the list
+     */
+    shift() {
+        if (!this.length) {
+            return false;
+        }
+
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            const oldHead = this.head;
+            this.head = oldHead.next;
+            this.head.previous = null;
+            oldHead.next = null;
+        }
+
+        this.length--;
+        return true;
+    }
+
+    /**
+     * Add node to the beginning of the list
+     * @param value
+     */
+    unshift(value) {
+        const newNode = new Node(value);
+
+        if (!this.length) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head.previous = newNode;
+            this.head = newNode;
+        }
+
+        this.length++;
+        return true;
+    }
+
+    /**
+     *  Accessing a node in list by its position
+     */
+    get(position) {
+        if (!this.length) {
+            return false;
+        }
+        if (position < 0 || position >= this.length) {
+            return false;
+        }
+        if (position === 0) {
+            return this.head
+        }
+        if (position === this.length - 1) {
+            return this.tail;
+        }
+
+        const middle = Math.floor(this.length / 2);
+        let current;
+        let counter;
+
+        if (position <= middle) {
+            current = this.head;
+            counter = 0;
+            while (position !== counter) {
+                current = current.next;
+                counter++;
+            }
+        } else {
+            current = this.tail;
+            counter = this.length - 1;
+            while (position !== counter) {
+                current = current.previous;
+                counter--;
+            }
+        }
+
+        return current;
+    }
+
+    /**
+     *  Replacing the value of a node in the list
+     */
+    set(value, position) {
+        const targetNode = this.get(position);
+        if (targetNode) {
+            targetNode.value = value;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     *  Adding a node in list by a certain position
+     */
+    insert(value, position) {
+        if(!this.length) {
+            return false;
+        }
+
+        if(position < 0 || position > this.length) {
+            return false;
+        }
+
+        if(position === 0) {
+            return this.unshift(value);
+        }else if(position === this.length) {
+            return this.push(value);
+        } else {
+            const newNode = new Node(value);
+            const targetNode = this.get(position - 1);
+
+            const previous = targetNode.previous;
+            const following = targetNode.next;
+
+            previous.next = newNode;
+            newNode.previous = previous;
+
+            following.previous = newNode;
+            newNode.next = following;
+
+            this.length++;
+        }
+
+        return true;
+    }
+}
